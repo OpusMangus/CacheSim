dd00d9fe88cacc8ae4a44795a5e9393c4067eddb
Joaquin Saavedra <joaq4699@hotmail.com>
2020-11-15 10:55:41 -0300

    Implementado parseFile en traceParser

--- simuladorcache.java
+++ simuladorcache.java
@@ -1,9 +1,26 @@
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class simuladorcache {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World.");
-        System.out.println(logTwo.log2(1));
-        System.out.println(String.format("%32s", hexToBin.convert("8")).replace(' ', '0'));
+        // System.out.println(logTwo.log2(1));
+        // System.out.println(String.format("%32s", hexToBin.convert("8")).replace(' ',
+        // '0'));
+
+        File file = new File("spice10.trace");
+
+        ArrayList<instruccion> instArr = null;
+        try {
+            instArr = traceParser.parseFile(file, 1, 10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        for (instruccion inst : instArr) {
+            System.out.println(inst);
+        }
     }
 }
--- traceParser.java
+++ traceParser.java
@@ -1,18 +1,37 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class traceParser {
-    private static instruccion parseLine(String s, int tagSize, int idSetSize){
+    public static instruccion parseLine(String s, int tagSize, int idSetSize) {
         String[] splitted = s.split(" ");
         instruccion inst = new instruccion();
-        
-        //Tipo
-        inst.setTipo(splitted[0]);
-        
-        String direccion = hexToBin32.convert(splitted[1]);
-        //Tag
-        inst.setTag(direccion.substring(0, tagSize - 1));
 
-        //idSet
+        // Tipo
+        inst.setTipo(splitted[0]);
+
+        String direccion = hexToBin32.convert(splitted[1]);
+        // Tag
+        inst.setTag(direccion.substring(0, tagSize));
+
+        // idSet
         inst.setIdSet(direccion.substring(tagSize, tagSize + idSetSize));
 
         return inst;
+    }
+
+    public static ArrayList<instruccion> parseFile(File file, int tagSize, int idSetSize) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        ArrayList<instruccion> instArr = new ArrayList<instruccion>();
+        String st;
+        while ((st = br.readLine()) != null){
+            instArr.add(traceParser.parseLine(st, tagSize, idSetSize));
+        }
+        br.close();
+
+        return instArr;
     }
 }
